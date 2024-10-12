10da14e088c89e94fd1e98dfb2b7d24de4fd8310
alina <blondinkaa27@gmail.com>
2024-10-12 16:33:48 +0300

    split to methods

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    // Задание 1
+    public static void task_1() {
         Scanner scanner = new Scanner(System.in);
 
-        // Задание 1
         System.out.println("Введите первое число:");
         int firstNumber = scanner.nextInt();
         System.out.println("Введите второе числo:");
@@ -30,15 +30,19 @@ public class Main {
         System.out.println("Разница " + difference);
         System.out.println("Умножение " + multiply);
         System.out.println("Деление " + division);
-
-        // Задание 2
+    }
+    // Задание 2
+    public static void task_2() {
         int[] arrayInteger = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int[] arrayEvenNumbers = Arrays.stream(arrayInteger)
                 .filter(number -> number % 2 == 0).toArray();
 
         System.out.println("Массив четный: " + Arrays.toString(arrayEvenNumbers));
+    }
+    // Задание 3
+    public static void task_3() {
+        Scanner scanner = new Scanner(System.in);
 
-        // Задание 3
         scanner.nextLine();
         System.out.println("Введите первую строку:");
         String firstString = scanner.nextLine();
@@ -53,6 +57,11 @@ public class Main {
             resultMessage += "неидентичны";
         }
         System.out.println(resultMessage);
+    }
+    public static void main(String[] args) {
+        task_1();
+        task_2();
+        task_3();
     }
 }
 
